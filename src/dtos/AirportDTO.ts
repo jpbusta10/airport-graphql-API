9564cb178239484840af3b84203662808d5bb8a7
solvd <solvd@solvds-MBP.fibertel.com.ar>
2023-07-20 11:56:49 -0300

    DTOs

---
+++ src/dtos/AirportDTO.ts
@@ -0,0 +1,32 @@
+class AirportDTO{
+    _airportCode: string;
+    _airPortCountry: string;
+    _gpsCode: string;
+
+    constructor(airportCode:string, airportCountry:string, gpsCode:string){
+        this._airportCode = airportCode;
+        this._airPortCountry = airportCountry;
+        this._gpsCode = gpsCode;
+    }
+
+    get airportCode(): string{
+        return this._airportCode;
+    }
+    get airportCountry():string{
+        return this._airPortCountry;
+    }
+    get gpsCode():string{
+        return this._gpsCode;
+    }
+
+    set airportCode(airportCode: string){
+        this._airportCode = airportCode;
+    }
+    set airportCountry(airportCountry: string){
+        this._airPortCountry = airportCountry;
+    }
+    set gpsCode(gpsCode:string){
+        this._gpsCode;
+    }
+
+}
--- src/dtos/passengerDTO.ts
+++ src/dtos/passengerDTO.ts
@@ -1,5 +1,47 @@
 class PassengerDTO {
-    private uuid: string;
-    private email: string;
-    private country_of_birth: string;
+    private _uuid: string;
+    private _firstName: string;
+    private _lastName: string;
+    private _email: string;
+    private _countryOfBirth: string;
+
+    constructor(firstName: string, lastName: string, email:string, countryOfBirth: string, uuid?: string){
+        this._uuid = uuid;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._email = email;
+        this._countryOfBirth = countryOfBirth;
+    }
+
+    get uuid(): string | undefined {
+        return this._uuid;
+    }
+
+    get firstName(): string {
+        return this._firstName;
+    }
+    
+    get lastName(): string {
+        return this._lastName;
+    }
+
+    get email(): string {
+        return this.email;
+    }
+    get countryOfBirth(): string {
+        return this._countryOfBirth
+    }
+    set firstName(firstName: string){
+        this._firstName = firstName;
+    }
+    set lastName(lastName: string){
+        this._lastName = lastName;
+    }
+    set email(email: string){
+        this._email = email;
+    }
+    set countryOfBirth(countryOfBirth: string){
+        this._countryOfBirth = countryOfBirth;
+    }
+
 }
